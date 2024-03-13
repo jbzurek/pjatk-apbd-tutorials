f2498528844d25ee45a0e72e0d767ac65eae4e24
jbzurek <s25434@pjwstk.edu.pl>
2024-03-13 23:02:37 +0100

    Finished feature-average

--- tutorial02/Program.cs
+++ tutorial02/Program.cs
@@ -4,8 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        string name = "Jakub";
-        string city = "Warsaw";
-        Console.WriteLine("Hello, " + name + " from " + city);
+        // string name = "Jakub";
+        // string city = "Warsaw";
+        // Console.WriteLine("Hello, " + name + " from " + city);
+
+        int[] array = [2, 2, 2, 3, 1, 1];
+        Console.WriteLine(CountAverage(array));
+    }
+
+    public static double CountAverage(int[] array)
+    {
+        double sum = 0;
+        for (int i = 0; i < array.Length; i++)
+        {
+            sum += array[i];
+        }
+
+        return sum / array.Length;
     }
 }
