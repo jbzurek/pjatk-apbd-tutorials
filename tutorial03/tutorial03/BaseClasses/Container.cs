cb25ddb647ddcc6a23536bebca0a720b820f6d81
Jakub Żurek <s25434@pjwstk.edu.pl>
2024-03-18 18:32:17 +0100

    Added Container.cs in tutorial03

---
+++ tutorial03/tutorial03/BaseClasses/Container.cs
@@ -0,0 +1,25 @@
+﻿namespace tutorial03.BaseClasses;
+
+public abstract class Container
+{
+    public int NetWeight { get; set; }
+    public int Height { get; set; }
+    public int TareWeight { get; set; }
+    public int Depth { get; set; }
+    public int MaxWeight { get; set; }
+    public string SerialNumber { get; set; }
+
+    public Container(int netWeight, int height, int tareWeight, int depth, int maxWeight, string serialNumber)
+    {
+        NetWeight = netWeight;
+        Height = height;
+        TareWeight = tareWeight;
+        Depth = depth;
+        MaxWeight = maxWeight;
+        SerialNumber = serialNumber;
+    }
+
+    public abstract void Unload();
+    public abstract void Load();
+    
+}
