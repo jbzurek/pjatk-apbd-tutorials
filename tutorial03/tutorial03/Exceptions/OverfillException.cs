e1e8463bc491212b765fd8bad68e0422425a2b7d
Jakub Żurek <s25434@pjwstk.edu.pl>
2024-03-20 17:43:16 +0100

    Added Exceptions

---
+++ tutorial03/tutorial03/Exceptions/OverfillException.cs
@@ -0,0 +1,9 @@
+﻿namespace tutorial03.Exceptions;
+
+public class OverfillException : Exception
+{
+    public OverfillException(string msg) : base(msg)
+    {
+        throw new Exception(msg);
+    }
+}
