b6f2b3bc90227cd359a977f231f209d9ccdf192f
Jakub Żurek <s25434@pjwstk.edu.pl>
2024-03-25 12:19:11 +0100

    Added LiquidContainer.cs

---
+++ tutorial03/tutorial03/Containers/LiquidContainer.cs
@@ -0,0 +1,21 @@
+﻿using tutorial03.BaseClasses;
+
+namespace tutorial03.Containers;
+
+public class LiquidContainer : Container
+{
+    public LiquidContainer(int netWeight, int height, int tareWeight, int depth, int maxWeight, string serialNumber) : base(netWeight, height, tareWeight, depth, maxWeight, serialNumber)
+    {
+        
+    }
+
+    public override void Unload()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void Load()
+    {
+        throw new NotImplementedException();
+    }
+}
