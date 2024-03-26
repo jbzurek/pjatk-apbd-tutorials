ff6f6500fca1d5977d45fa7e069062e6ef16628a
Jakub Żurek <s25434@pjwstk.edu.pl>
2024-03-26 11:42:00 +0100

    Added WeightExceedException.cs

---
+++ tutorial03/tutorial03/Exceptions/WeightExceedException.cs
@@ -0,0 +1,9 @@
+﻿namespace tutorial03.Exceptions;
+
+public class WeightExceedException : Exception
+{
+    public WeightExceedException(string message) : base(message)
+    {
+        throw new Exception(message);
+    }
+}
