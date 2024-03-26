2ac796e1f7d30af8b2115d36f157cc3484bba701
Jakub Żurek <s25434@pjwstk.edu.pl>
2024-03-26 11:21:05 +0100

    Modified Container.cs

--- tutorial03/tutorial03/BaseClasses/Container.cs
+++ tutorial03/tutorial03/BaseClasses/Container.cs
@@ -1,4 +1,6 @@
-﻿namespace tutorial03.BaseClasses;
+﻿using tutorial03.Generators;
+
+namespace tutorial03.BaseClasses;
 
 public abstract class Container
 {
@@ -8,15 +10,16 @@ public abstract class Container
     public int Depth { get; set; }
     public int MaxWeight { get; set; }
     public string SerialNumber { get; set; }
+    public abstract string ContainerType { get; }
 
-    public Container(int netWeight, int height, int tareWeight, int depth, int maxWeight, string serialNumber)
+    public Container(int netWeight, int height, int tareWeight, int depth, int maxWeight, ContainerSerialNumberGenerator serialNumberGenerator)
     {
         NetWeight = netWeight;
         Height = height;
         TareWeight = tareWeight;
         Depth = depth;
         MaxWeight = maxWeight;
-        SerialNumber = serialNumber;
+        SerialNumber = serialNumberGenerator.GenerateSerialNumber(ContainerType);
     }
 
     public abstract void Unload();
