4a94cb83a18f96ab3b124075f937195950aecea5
Jakub Żurek <s25434@pjwstk.edu.pl>
2024-04-11 14:27:34 +0200

    Modified working directory

--- tutorial03/Program.cs
+++ tutorial03/Program.cs
@@ -6,10 +6,9 @@ public class Program
 {
     public static void Main(string[] args)
     {
-
         ContainerShip ship1 = new ContainerShip("Black Pearl", 20, 10, 100000);
         ContainerShip ship2 = new ContainerShip("Santa Maria", 40, 30, 300000);
-        
+
         Menu menu = new Menu();
         menu.DisplayMainMenu();
     }
