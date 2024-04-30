22583d6a382c1fe07ccdbc2931b3096e6fec90b4
Jakub Żurek <s25434@pjwstk.edu.pl>
2024-04-30 17:01:31 +0200

    Add images folder in tutorial07 folder

--- tutorial07/obj/Debug/net8.0/tutorial07.AssemblyInfo.cs
+++ tutorial07/obj/Debug/net8.0/tutorial07.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tutorial07")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e9121efef56a2cb9e67b29d6ad7614c8dbdbf1d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("tutorial07")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tutorial07")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
