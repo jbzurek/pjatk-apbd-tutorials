81eda98124c07ec6cf8fa7743e6211ccf1945b87
Jakub Żurek <s25434@pjwstk.edu.pl>
2024-06-24 13:08:38 +0200

    Delete files

--- sample-test02/Models/BoatStandard.cs
+++
@@ -1,8 +0,0 @@
-namespace sample_test02.Models;
-
-public class BoatStandard
-{
-    public int Id { get; set; }
-    public int Level { get; set; }
-    public int AvailableBoats { get; set; }
-}
--- sample-test02/Models/Promotion.cs
+++
@@ -1,9 +0,0 @@
-namespace sample_test02.Models;
-
-public class Promotion
-{
-    public int Id { get; set; }
-    public string Code { get; set; }
-    public decimal Discount { get; set; }
-    public ICollection<Client> Clients { get; set; }
-}
